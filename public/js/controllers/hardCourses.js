18a2e8dce91255d7333894e86655d752286e78df
Chester O'Neill <c.oneill.codes@gmail.com>
2014-02-20 07:10:18 -0500

    refactoring

--- public/js/controllers/hardCourses.js
+++ public/js/controllers/hardCourses.js
@@ -1,41 +1,41 @@
 'use strict';
 
 angular.module('mean.articles').controller('HardCoursesController', ['$scope', '$routeParams', '$location', 'Global', 'HardCourses', function ($scope, $routeParams, $location, Global, HardCourses) {
-    $scope.global = Global;
+	$scope.global = Global;
 
-    $scope.get = function() {
-        HardCourses.get({
-            courseCd: $routeParams.courseCd
-        }, function(course) {
-            $scope.course = course;
-        });
-    };
+	$scope.get = function() {
+		HardCourses.get({
+			courseCd: $routeParams.courseCd
+		}, function(course) {
+			$scope.course = course;
+		});
+	};
 
-    $scope.find = function() {
-        HardCourses.get({
-            courseCd: $scope.courseCd
-        }, function(course) {
-            $scope.course = course;
-            var chart = [];
-            for(var i=0; i < course.results.length; i++) {
-            	var data = {};
-            	data.datasets = [];
-            	data.labels = [];
-            	var results = course.results[i].results;
-            	data.datasets.push({
-					fillColor : "rgba(220,220,220,0.5)",
-					strokeColor : "rgba(220,220,220,1)",
+	$scope.find = function() {
+		HardCourses.get({
+			courseCd: $scope.courseCd
+		}, function(course) {
+			$scope.course = course;
+			var chart = [];
+			for(var i=0; i < course.results.length; i++) {
+				var data = {};
+				data.datasets = [];
+				data.labels = [];
+				var results = course.results[i].results;
+				data.datasets.push({
+					fillColor : 'rgba(220,220,220,0.5)',
+					strokeColor : 'rgba(220,220,220,1)',
 					data : []
 				});
-        		for(var j=0; j < results.length; j++) {
-        			var result = results[j];
-    				data.labels.push(result.credits);
-    				data.datasets[0].data.push(result.times);
-            	}
-            	chart.push(data);
-            }
-            
-            $scope.chart = chart;
-        });
-    };
+				for(var j=0; j < results.length; j++) {
+					var result = results[j];
+					data.labels.push(result.credits);
+					data.datasets[0].data.push(result.times);
+				}
+				chart.push(data);
+			}
+
+			$scope.chart = chart;
+		});
+	};
 }]);
--- public/js/directives.js
+++ public/js/directives.js
@@ -1,10 +1,10 @@
 'use strict';
 /* General Chart Wrapper */
-angular.module('mean').directive('chart', function () {
+angular.module('mean').directive('cjChart', function () {
 	return {
 		restrict: 'A',
 		link: function (scope, elem) {
-			var ctx = elem[0].getContext("2d");
+			var ctx = elem[0].getContext('2d');
 
 			var chart = new Chart(ctx).Bar(scope.chart[scope.$index]);
 
